08ad82c1e01edfbc050d85a8f55f447bcab570a3
Ole-Martin Mørk <olemartin@gmail.com>
2016-10-26 15:07:46 +0200

    Lagt til script for tensorboard og eksponert port fra docker

--- env.sh
+++ env.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 SRC_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 SRC_NAME="${PWD##*/}"
-docker run -itp 8888:8888 --entrypoint /bin/bash -v $SRC_PATH:/$SRC_NAME gcr.io/tensorflow/tensorflow:0.11.0rc0
+docker run -it -p 8888:8888 -p 6006:6006 --entrypoint /bin/bash -v $SRC_PATH:/$SRC_NAME gcr.io/tensorflow/tensorflow:0.11.0rc0
---
+++ tensorboard.sh
@@ -0,0 +1,6 @@
+echo '##################################################'
+echo '#                                                #'
+echo '# Tensorboard will run at http://localhost:6006  #'
+echo '#                                                #'
+echo '##################################################'
+tensorboard --port 6006 --logdir 1-machine-learning/log
