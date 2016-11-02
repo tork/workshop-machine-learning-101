f991548208ee20e119585e16a474d780c58b2b62
Ole-Martin Mørk <olemartin@gmail.com>
2016-11-02 16:20:40 +0100

    Restrukturert nmist

--- 1-machine-learning/solution/network.py
+++ 1-machine-learning/solution/network.py
@@ -1,7 +1,7 @@
 import input_data
 import tensorflow as tf
 
-mnist = input_data.read_data_sets("../data/mnist", one_hot=True)
+mnist = input_data.read_data_sets("../../data/mnist", one_hot=True)
 
 print("Finished extracting data")
 
